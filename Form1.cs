62f8294029c5e3c4c201872b7d3790f2cdf47c03
Haidang25 <nguyenhaidang18625@gmail.com>
2026-08-21 12:25:40 +0000

    Dat ten ung dung, icon va thong tin phien ban
    - AssemblyInfo.cs: AssemblyTitle/AssemblyProduct = SFileManager, version giu 1.0.0.0
    - csproj: ApplicationIcon = Resources/app.ico
    - Resources/app.ico: icon placeholder (folder icon)
    - Form1: set Text = SFileManager, dung icon cua exe lam icon form

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileExplorerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "SFileManager";
+            // Dung icon da gan cho file .exe (ApplicationIcon) lam icon cua form,
+            // khong phu thuoc duong dan tuong doi luc chay.
+            this.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+        }
+    }
+}
