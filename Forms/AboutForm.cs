ad3f843dd8bf81c05bc62367199e77fd5a1a736c
Haidang25 <nguyenhaidang18625@gmail.com>
2026-08-22 18:12:07 +0000

    Tao LogForm va AboutForm rong, them vao csproj

---
+++ Forms/AboutForm.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace FileExplorerApp.Forms
+{
+    /// <summary>
+    /// Form rong, du kien thay the hop thoai About hien dang hien bang MessageBox trong
+    /// MainForm.mnuHelpAbout_Click. Hien tai chua co noi dung/control gi - se bo sung
+    /// (ten san pham, phien ban, ban quyen...) va noi voi mnuHelpAbout_Click khi co
+    /// yeu cau cu the.
+    /// </summary>
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Forms/LogForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace FileExplorerApp.Forms
+{
+    /// <summary>
+    /// Form rong, du kien dung de xem lich su thao tac (menu Cong cu > Xem nhat ky
+    /// hoat dong), ket hop voi LogService.GetLogs(). Hien tai chua co noi dung/control
+    /// gi - se bo sung khi co yeu cau cu the (VD: ListView liet ke LogEntryModel).
+    /// </summary>
+    public partial class LogForm : Form
+    {
+        public LogForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
