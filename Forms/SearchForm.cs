745cbb72e7719fea7e33caa13dfb88f4374d6cc5
Haidang25 <nguyenhaidang18625@gmail.com>
2026-08-22 18:10:53 +0000

    Tao SearchForm rong (chua noi dung), them vao csproj

---
+++ Forms/SearchForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace FileExplorerApp.Forms
+{
+    /// <summary>
+    /// Form rong, du kien dung de tim kiem file/thu muc (ket hop voi SearchService)
+    /// tu MainForm (menu Cong cu > Tim kiem...). Hien tai chua co noi dung/control
+    /// gi - se bo sung khi co yeu cau cu the (o nhap tu khoa, bo loc, ket qua...).
+    /// </summary>
+    public partial class SearchForm : Form
+    {
+        public SearchForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
