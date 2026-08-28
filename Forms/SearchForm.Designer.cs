e8ed5e99ae0dba36a799c1af5e4a3fa896d910dd
Haidang25 <nguyenhaidang18625@gmail.com>
2026-08-28 00:37:21 +0000

    Thiet ke SearchForm: o tu khoa, thu muc goc + chon thu muc, tuy chon de quy/muc an, ket qua dang ListView, ho tro Huy

--- Forms/SearchForm.Designer.cs
+++ Forms/SearchForm.Designer.cs
@@ -7,6 +7,24 @@ namespace FileExplorerApp.Forms
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
+        private System.Windows.Forms.Label lblKeyword;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.Label lblRootFolder;
+        private System.Windows.Forms.TextBox txtRootFolder;
+        private System.Windows.Forms.Button btnBrowseRootFolder;
+        private System.Windows.Forms.GroupBox grpOptions;
+        private System.Windows.Forms.CheckBox chkRecursive;
+        private System.Windows.Forms.CheckBox chkIncludeHidden;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnCancelSearch;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ListView lvwResults;
+        private System.Windows.Forms.ColumnHeader colResultName;
+        private System.Windows.Forms.ColumnHeader colResultLocation;
+        private System.Windows.Forms.ColumnHeader colResultSize;
+        private System.Windows.Forms.ColumnHeader colResultModified;
+        private System.Windows.Forms.Button btnClose;
+
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
@@ -28,21 +46,204 @@ namespace FileExplorerApp.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblKeyword = new System.Windows.Forms.Label();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.lblRootFolder = new System.Windows.Forms.Label();
+            this.txtRootFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowseRootFolder = new System.Windows.Forms.Button();
+            this.grpOptions = new System.Windows.Forms.GroupBox();
+            this.chkRecursive = new System.Windows.Forms.CheckBox();
+            this.chkIncludeHidden = new System.Windows.Forms.CheckBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnCancelSearch = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lvwResults = new System.Windows.Forms.ListView();
+            this.colResultName = new System.Windows.Forms.ColumnHeader();
+            this.colResultLocation = new System.Windows.Forms.ColumnHeader();
+            this.colResultSize = new System.Windows.Forms.ColumnHeader();
+            this.colResultModified = new System.Windows.Forms.ColumnHeader();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.grpOptions.SuspendLayout();
             this.components = new System.ComponentModel.Container();
             this.SuspendLayout();
-
+            //
+            // lblKeyword
+            //
+            this.lblKeyword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblKeyword.Location = new System.Drawing.Point(16, 16);
+            this.lblKeyword.Name = "lblKeyword";
+            this.lblKeyword.Size = new System.Drawing.Size(200, 20);
+            this.lblKeyword.TabIndex = 0;
+            this.lblKeyword.Text = "Từ khóa:";
+            //
+            // txtKeyword
+            //
+            this.txtKeyword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtKeyword.Location = new System.Drawing.Point(16, 38);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(668, 23);
+            this.txtKeyword.TabIndex = 1;
+            //
+            // lblRootFolder
+            //
+            this.lblRootFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblRootFolder.Location = new System.Drawing.Point(16, 70);
+            this.lblRootFolder.Name = "lblRootFolder";
+            this.lblRootFolder.Size = new System.Drawing.Size(200, 20);
+            this.lblRootFolder.TabIndex = 2;
+            this.lblRootFolder.Text = "Thư mục gốc:";
+            //
+            // txtRootFolder
+            //
+            this.txtRootFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRootFolder.Location = new System.Drawing.Point(16, 92);
+            this.txtRootFolder.Name = "txtRootFolder";
+            this.txtRootFolder.Size = new System.Drawing.Size(580, 23);
+            this.txtRootFolder.TabIndex = 3;
+            //
+            // btnBrowseRootFolder
+            //
+            this.btnBrowseRootFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseRootFolder.Location = new System.Drawing.Point(604, 91);
+            this.btnBrowseRootFolder.Name = "btnBrowseRootFolder";
+            this.btnBrowseRootFolder.Size = new System.Drawing.Size(64, 25);
+            this.btnBrowseRootFolder.TabIndex = 4;
+            this.btnBrowseRootFolder.Text = "...";
+            this.btnBrowseRootFolder.Click += new System.EventHandler(this.btnBrowseRootFolder_Click);
+            //
+            // grpOptions
+            //
+            this.grpOptions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpOptions.Controls.Add(this.chkRecursive);
+            this.grpOptions.Controls.Add(this.chkIncludeHidden);
+            this.grpOptions.Location = new System.Drawing.Point(16, 128);
+            this.grpOptions.Name = "grpOptions";
+            this.grpOptions.Size = new System.Drawing.Size(668, 72);
+            this.grpOptions.TabIndex = 5;
+            this.grpOptions.TabStop = false;
+            this.grpOptions.Text = "Tùy chọn";
+            //
+            // chkRecursive
+            //
+            this.chkRecursive.Checked = true;
+            this.chkRecursive.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkRecursive.Location = new System.Drawing.Point(16, 24);
+            this.chkRecursive.Name = "chkRecursive";
+            this.chkRecursive.Size = new System.Drawing.Size(320, 24);
+            this.chkRecursive.TabIndex = 0;
+            this.chkRecursive.Text = "Tìm cả trong thư mục con";
+            //
+            // chkIncludeHidden
+            //
+            this.chkIncludeHidden.Location = new System.Drawing.Point(16, 44);
+            this.chkIncludeHidden.Name = "chkIncludeHidden";
+            this.chkIncludeHidden.Size = new System.Drawing.Size(320, 24);
+            this.chkIncludeHidden.TabIndex = 1;
+            this.chkIncludeHidden.Text = "Bao gồm mục ẩn/hệ thống";
+            //
+            // btnSearch
+            //
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSearch.Location = new System.Drawing.Point(16, 212);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(120, 32);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            //
+            // btnCancelSearch
+            //
+            this.btnCancelSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCancelSearch.Enabled = false;
+            this.btnCancelSearch.Location = new System.Drawing.Point(144, 212);
+            this.btnCancelSearch.Name = "btnCancelSearch";
+            this.btnCancelSearch.Size = new System.Drawing.Size(120, 32);
+            this.btnCancelSearch.TabIndex = 7;
+            this.btnCancelSearch.Text = "Hủy";
+            this.btnCancelSearch.Click += new System.EventHandler(this.btnCancelSearch_Click);
+            //
+            // lblStatus
+            //
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatus.Location = new System.Drawing.Point(280, 218);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(404, 20);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "Sẵn sàng";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            //
+            // lvwResults
+            //
+            this.lvwResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvwResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colResultName,
+            this.colResultLocation,
+            this.colResultSize,
+            this.colResultModified});
+            this.lvwResults.FullRowSelect = true;
+            this.lvwResults.Location = new System.Drawing.Point(16, 256);
+            this.lvwResults.Name = "lvwResults";
+            this.lvwResults.Size = new System.Drawing.Size(668, 268);
+            this.lvwResults.TabIndex = 9;
+            this.lvwResults.UseCompatibleStateImageBehavior = false;
+            this.lvwResults.View = System.Windows.Forms.View.Details;
+            //
+            // colResultName
+            //
+            this.colResultName.Text = "Tên";
+            this.colResultName.Width = 220;
+            //
+            // colResultLocation
+            //
+            this.colResultLocation.Text = "Vị trí";
+            this.colResultLocation.Width = 230;
+            //
+            // colResultSize
+            //
+            this.colResultSize.Text = "Kích thước";
+            this.colResultSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.colResultSize.Width = 100;
+            //
+            // colResultModified
+            //
+            this.colResultModified.Text = "Ngày sửa";
+            this.colResultModified.Width = 118;
+            //
+            // btnClose
+            //
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(584, 532);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 32);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Đóng";
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             //
             // SearchForm
             //
+            this.AcceptButton = this.btnSearch;
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = FileExplorerApp.Helpers.AppTheme.Background;
-            this.ClientSize = new System.Drawing.Size(500, 400);
+            this.ClientSize = new System.Drawing.Size(700, 580);
+            this.Controls.Add(this.lblKeyword);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.lblRootFolder);
+            this.Controls.Add(this.txtRootFolder);
+            this.Controls.Add(this.btnBrowseRootFolder);
+            this.Controls.Add(this.grpOptions);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnCancelSearch);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lvwResults);
+            this.Controls.Add(this.btnClose);
             this.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.ForeColor = FileExplorerApp.Helpers.AppTheme.TextPrimary;
             this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(560, 420);
             this.Name = "SearchForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "SearchForm";
+            this.Text = "Tìm kiếm";
+            this.grpOptions.ResumeLayout(false);
             this.ResumeLayout(false);
         }
 
--- Forms/SearchForm.cs
+++ Forms/SearchForm.cs
@@ -1,17 +1,153 @@
-﻿using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FileExplorerApp.Helpers;
+using FileExplorerApp.Models;
+using FileExplorerApp.Services;
 
 namespace FileExplorerApp.Forms
 {
     /// <summary>
-    /// Form rong, du kien dung de tim kiem file/thu muc (ket hop voi SearchService)
-    /// tu MainForm (menu Cong cu > Tim kiem...). Hien tai chua co noi dung/control
-    /// gi - se bo sung khi co yeu cau cu the (o nhap tu khoa, bo loc, ket qua...).
+    /// Man hinh tim kiem file/thu muc (menu Cong cu > Tim kiem..., hoac Enter tren o
+    /// tim kiem cua thanh cong cu MainForm). Nhap tu khoa + thu muc goc + tuy chon,
+    /// goi SearchService.Search() tren luong nen (Task.Run) de khong lam treo UI, ho
+    /// tro Huy giua chung qua CancellationTokenSource - cung mau voi CopyProgressForm/
+    /// mnuEditPaste_Click da lam voi thao tac Copy.
     /// </summary>
     public partial class SearchForm : Form
     {
-        public SearchForm()
+        private readonly SearchService _searchService = new SearchService();
+
+        // Chi khac null trong luc dang co mot lan tim kiem dang chay - dung de nut
+        // Huy (btnCancelSearch_Click) co the goi Cancel(); null lai ngay khi tim
+        // kiem ket thuc (thanh cong, loi, hay bi huy) de tranh goi Cancel() tren mot
+        // CancellationTokenSource da Dispose().
+        private CancellationTokenSource _searchCts;
+
+        public SearchForm() : this(rootFolder: null, keyword: null)
+        {
+        }
+
+        /// <summary>
+        /// Khoi tao SearchForm voi thu muc goc va tu khoa co san (VD: MainForm truyen
+        /// san _currentPath va noi dung dang go trong o tim kiem cua thanh cong cu).
+        /// </summary>
+        /// <param name="rootFolder">Duong dan thu muc goc dien san vao txtRootFolder. Bo qua neu null/rong.</param>
+        /// <param name="keyword">Tu khoa dien san vao txtKeyword. Bo qua neu null/rong.</param>
+        public SearchForm(string rootFolder, string keyword = null)
         {
             InitializeComponent();
+
+            if (!string.IsNullOrWhiteSpace(rootFolder))
+                txtRootFolder.Text = rootFolder;
+
+            if (!string.IsNullOrWhiteSpace(keyword))
+                txtKeyword.Text = keyword;
+        }
+
+        private void btnBrowseRootFolder_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (!string.IsNullOrWhiteSpace(txtRootFolder.Text) && Directory.Exists(txtRootFolder.Text))
+                    dialog.SelectedPath = txtRootFolder.Text;
+
+                if (dialog.ShowDialog(this) == DialogResult.OK)
+                    txtRootFolder.Text = dialog.SelectedPath;
+            }
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            string keyword = txtKeyword.Text.Trim();
+            string rootFolder = txtRootFolder.Text.Trim();
+
+            if (string.IsNullOrEmpty(keyword))
+            {
+                MessageBox.Show(this, "Nhập từ khóa cần tìm.", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtKeyword.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(rootFolder) || !Directory.Exists(rootFolder))
+            {
+                MessageBox.Show(this, "Thư mục gốc không hợp lệ hoặc không tồn tại.", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtRootFolder.Focus();
+                return;
+            }
+
+            bool recursive = chkRecursive.Checked;
+            bool includeHidden = chkIncludeHidden.Checked;
+
+            lvwResults.Items.Clear();
+            lblStatus.Text = "Đang tìm kiếm...";
+            btnSearch.Enabled = false;
+            btnCancelSearch.Enabled = true;
+
+            using (_searchCts = new CancellationTokenSource())
+            {
+                CancellationToken token = _searchCts.Token;
+                try
+                {
+                    // SearchService.Search() la ham dong bo (co the mat thoi gian voi
+                    // thu muc lon) - chay qua Task.Run de khong chan UI thread, giong
+                    // ly do CopyFileAsync/CopyFolderAsync duoc thiet ke bat dong bo.
+                    List<FileItemModel> results = await Task.Run(
+                        () => _searchService.Search(rootFolder, keyword, recursive, includeHidden, token), token);
+
+                    PopulateResults(results);
+                    lblStatus.Text = $"Tìm thấy {results.Count} mục.";
+                }
+                catch (OperationCanceledException)
+                {
+                    lblStatus.Text = "Đã hủy tìm kiếm.";
+                }
+                finally
+                {
+                    btnSearch.Enabled = true;
+                    btnCancelSearch.Enabled = false;
+                    _searchCts = null;
+                }
+            }
+        }
+
+        private void btnCancelSearch_Click(object sender, EventArgs e)
+        {
+            // Vo hieu hoa ngay de tranh bam nhieu lan trong luc cho vong lap trong
+            // SearchService.SearchRecursive kip kiem tra ThrowIfCancellationRequested().
+            btnCancelSearch.Enabled = false;
+            _searchCts?.Cancel();
+        }
+
+        /// <summary>Do danh sach ket qua vao lvwResults, dung BeginUpdate/EndUpdate de tranh nhap nhay khi co nhieu ket qua.</summary>
+        private void PopulateResults(List<FileItemModel> results)
+        {
+            lvwResults.BeginUpdate();
+            try
+            {
+                foreach (FileItemModel item in results)
+                {
+                    var listItem = new ListViewItem(item.Name) { Tag = item.FullPath };
+                    listItem.SubItems.Add(item.ParentPath);
+                    listItem.SubItems.Add(item.IsDirectory ? string.Empty : item.SizeFormatted);
+                    listItem.SubItems.Add(FormatHelper.FormatDate(item.ModifiedDate));
+                    lvwResults.Items.Add(listItem);
+                }
+            }
+            finally
+            {
+                lvwResults.EndUpdate();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
